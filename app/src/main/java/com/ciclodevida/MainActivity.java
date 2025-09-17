73495c241b2f22f9900d5e4bdca02311c5800634
lucaslferreira <lucasllf2014@gmail.com>
2025-09-17 16:46:45 -0300

    add color, create button.

--- app/src/main/java/com/ciclodevida/MainActivity.java
+++ app/src/main/java/com/ciclodevida/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ciclodevida;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.ciclodevida.model.CiclodeVida;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView;
+    private CicloDeVida cicloDeVida;
+    private TextView tvConteudo;
+    private int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +28,61 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        textViewLog = findViewById(R.id.textViewLog);
+        tvConteudo = findViewById(R.id.tv_conteudo);
 
-
-        if (CicloDeVida.podeDizerOvalor()) {
-            x = CicloDeVida.digaOvalor();
+        if (CicloDeVida.podeDizerValor()) {
+            x = CicloDeVida.digaValor();
         }
 
-        CicloDeVida = CicloDeVida.getInstance();
-        cicloDeVida.registraEstado(msg"onCreate, x = " + x);
+        cicloDeVida = CicloDeVida.getInstance();
+        cicloDeVida.registraEstado("onCreate, x = " + x);
+
         mostraConteudo();
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cicloDeVida.registraEstado("onDestroy, x = " + x);
+        mostraConteudo();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cicloDeVida.registraEstado("onStart, x = " + x);
+        mostraConteudo();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cicloDeVida.registraEstado("onStop, x = " + x);
+        mostraConteudo();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cicloDeVida.registraEstado("onPause, x = " + x);
+        mostraConteudo();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cicloDeVida.registraEstado("onResume, x = " + x);
+        mostraConteudo();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        cicloDeVida.registraEstado("onRestart, x = " + x);
+        mostraConteudo();
     }
 
     private void mostraConteudo() {
-        textViewLog.setText(cicloDeVida)
+        tvConteudo.setText(cicloDeVida.toString());
     }
 }
